939cf7119c4d31a047ed1dde25897fb7aae2c6aa
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2020-09-20 17:16:51 +0200

    upgrade using latest drom

--- docs/sphinx/_static/documentation_options.js
+++ docs/sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (99e4bf5) (2020/08/27 10:36)',
+    VERSION: 'z-2020-09-20-upgrade-with-drom (acafb94) (2020/09/20 17:16)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- sphinx/index.rst
+++ sphinx/index.rst
@@ -4,8 +4,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to ez_config doc
-=================
+Welcome to ez_config
+================
 
 .. toctree::
    :maxdepth: 2
