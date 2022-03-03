d6d002bc833fc6ca03fdb026e9eec638a78972d0
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2022-03-03 12:09:48 +0100

    remove docs folder, use ez_file.0.3.0, tag version 0.2.0

--- src/ez_config/simpleConfig.mli
+++ src/ez_config/simpleConfig.mli
@@ -11,18 +11,7 @@
 (**************************************************************************)
 
 open EzCompat
-
-(**************************************************************************)
-(*                                                                        *)
-(*   Typerex Libraries                                                    *)
-(*                                                                        *)
-(*   Copyright 2011-2017 OCamlPro SAS                                     *)
-(*                                                                        *)
-(*   All rights reserved.  This file is distributed under the terms of    *)
-(*   the GNU Lesser General Public License version 2.1, with the          *)
-(*   special exception on linking described in the file LICENSE.          *)
-(*                                                                        *)
-(**************************************************************************)
+open Ez_file.V1
 
 type config_file
 
